155cdeecd4c35415abfed186c06ae5fe8fdaf86f
robertbarroso <r0230323@ucf.edu>
2026-06-20 11:25:55 -0400

    ADDED: Java Spring Boot and Maven

---
+++ backend/src/main/java/com/leaflet/leaflet_backend/LeafletBackendApplication.java
@@ -0,0 +1,13 @@
+package com.leaflet.leaflet_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeafletBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeafletBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/leaflet/leaflet_backend/LeafletBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.leaflet.leaflet_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeafletBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
